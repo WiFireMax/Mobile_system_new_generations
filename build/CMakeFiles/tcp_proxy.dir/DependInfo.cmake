42f4d93680149c189dbcf71573226438309782e8
WiFireMax <maxbakharev.nsk@gmail.com>
2024-11-26 09:20:07 +0700

    Final_first_matlab_lab

---
+++ build/CMakeFiles/tcp_proxy.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/firemax/Mobile_system_new_generations/tests/tcp_proxy.cpp" "CMakeFiles/tcp_proxy.dir/tests/tcp_proxy.cpp.o" "gcc" "CMakeFiles/tcp_proxy.dir/tests/tcp_proxy.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- tests/server_matlab_proxy.m
+++ tests/server_matlab_proxy.m
@@ -1,94 +1,67 @@
-function proxy_server()
-    % Укажите путь к JeroMQ JAR файлу
-    JARPATH = 'jeromq/target/jeromq-0.4.4-SNAPSHOT.jar';
-    javaclasspath(JARPATH);
-    
-    % Импорт необходимых классов
-    import org.zeromq.*;
-    
-    % Создание контекста и сокетов
-    context = ZContext();
-    socket_enb = context.createSocket(SocketType.REP); % Сокет для srsENB
-    socket_ue = context.createSocket(SocketType.REP);  % Сокет для srsUE
-    
-    % Привязка сокетов к портам
-    socket_enb.bind('tcp://*:3000'); % Порт для srsENB
-    socket_ue.bind('tcp://*:3001');  % Порт для srsUE
-    
-    disp('Proxy server запущен.');
+clear java;
+javaaddpath('home/firemax/Mobile_system_new_generations/src/jeromq-0.6.0')
 
-    while true
-        % Чтение данных от srsENB
-        msg_enb = socket_enb.recv(0);
-        fprintf('Received from srsENB: [%s]\n', char(msg_enb(:)'));
-        
-        % Отправка данных к srsUE
-        if ~isempty(msg_enb)
-            send_samples(socket_ue, msg_enb);
-        else
-            fprintf('No message received from srsENB.\n');
-        end
-        
-        % Чтение данных от srsUE
-        msg_ue = socket_ue.recv(0);
-        fprintf('Received from srsUE: [%s]\n', char(msg_ue(:)'));
-        
-        % Отправка данных обратно к srsENB (если необходимо)
-        if ~isempty(msg_ue)
-            socket_enb.send(msg_ue, 0);
-        else
-            fprintf('No message received from srsUE.\n');
-        end
-        
-        % Визуализация данных на графике
-        plot_data(msg_enb, msg_ue);
-    end
+import org.zeromq.ZMQ.*;
+import org.zeromq.*;
 
-    % Закрытие сокетов (не достигнет этой строки в бесконечном цикле)
-    socket_enb.close();
-    socket_ue.close();
-end
+port_api = 2111;
+context = ZMQ.context(1);
+socket_api_proxy = context.socket(ZMQ.REP);
+socket_api_proxy.bind(sprintf('tcp://*:%d', port_api));
 
-function send_samples(socket, samples)
-    % Преобразование данных в нужный формат для отправки
-    zmq_msg = java.nio.ByteBuffer.allocate(length(samples));
-    zmq_msg.put(samples);
+fprintf("Start")
+figure(1);
+global pauseFlag;
+pauseFlag = false;
+uicontrol('Style', 'pushbutton', 'String', 'Pause/Resume', ...
+              'Position', [20, 20, 100, 30], ...
+              'Callback', @(src, event) togglePause());
+while true
     
-    try
-        % Отправка сообщения через сокет
-        rc = socket.send(zmq_msg.array(), 0);
-        
-        if rc == -1
-            error('Ошибка при отправке сэмплов');
+    if ~pauseFlag
+        msg = socket_api_proxy.recv();
+        if ~isempty(msg)
+            fprintf('received message [%d]\n', length(msg));
+            if(length(msg) > 1000)
+                process_data(msg);
+            end
+            socket_api_proxy.send("OK");
         end
-    catch ME
-        fprintf('Error sending samples: %s\n', ME.message);
+    else
+        pause(0.1);
     end
 end
-
-function plot_data(data_enb, data_ue)
-    % Преобразование данных в числовой формат для визуализации
-    samples_enb = str2num(char(data_enb(:)')); %#ok<ST2NM>
-    samples_ue = str2num(char(data_ue(:)')); %#ok<ST2NM>
-    
-    % Визуализация данных на графике
-    figure(1);
-    hold on;
-    
-    t_enb = 1:length(samples_enb);
-    t_ue = 1:length(samples_ue);
-    
-    subplot(2, 1, 1);
-    plot(t_enb, samples_enb);
-    title('Сэмплы от srsENB');
-    xlabel('Время');
+function togglePause()
+    global pauseFlag;
+    pauseFlag = ~pauseFlag;
+end
+function process_data(data_raw)
+    fs = 23040000;
+    fprintf("size data: %d\n", length(data_raw));
+    data_slice = data_raw;
+    floatArray = typecast(uint8(data_slice), 'single');
+    complexArray = complex(floatArray(1:2:end), floatArray(2:2:end));
+    data_complex = complexArray(1:128*180);
+    fprintf("size complex data: %d\n", length(data_complex));
+    cla;
+    window = 128;    
+    noverlap = 0; 
+    nfft = 128;      
+    if any(isnan(data_complex))
+        data_complex(isnan(data_complex)) = 0;
+    end
+    subplot(2, 2, 1);
+    x_t = 1:length(data_complex);
+    plot(x_t, data_complex);
+    title('Данные в временной области');
+    xlabel('Отсчеты');
     ylabel('Амплитуда');
-    
-    subplot(2, 1, 2);
-    plot(t_ue, samples_ue);
-    title('Сэмплы от srsUE');
-    xlabel('Время');
-    ylabel('Амплитуда');
-    
-    drawnow; % Обновление графика
+    subplot(2, 2, 2);
+    spectrogram(data_complex, window, noverlap, nfft, fs, 'yaxis');
+    title('Спектрограмма переданных данных');
+    xlabel('Время (сек)');
+    ylabel('Частота (Гц)');
+    colorbar;
+    grid on;
+    drawnow;
 end
